3556091d47e5a4b3c356aa35e179f2ff6ca5ae77
GlennAntonySheen <glennantonysheen@gmail.com>
2023-04-15 12:24:22 +0100

    📝 SME can register themself

--- data/localweb/SME/application/views/header.php
+++ data/localweb/SME/application/views/header.php
@@ -1,3 +1,8 @@
+<?php
+// echo "Favorite color is " . $_SESSION["userType"] . ".<br>";
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
--- data/localweb/SME/application/views/login.php
+++ data/localweb/SME/application/views/login.php
@@ -1,15 +1,38 @@
 <?php
-session_start(); 
+session_start();
 $_SESSION['data'] = "yourdata";
 $message = "";
 class foo
 {public $name;}
 
+if (isset($_POST['SubmitSmeSignupBtn'])) {
+    $company_name = $_POST['company_name'];
+    $phone_no = $_POST['phone_no'];
+    $address = $_POST['address'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $conn = mysqli_connect('localhost', 'root', '', 'cw2');
+
+    $qry1 = "INSERT INTO login (email, password, userType) VALUES (\"" . $email . '" , "' . $password . '", ' . '"sme");';
+    if ($conn->query($qry1) === true) {
+        $qry2 = "INSERT INTO sme (sme_id, company_name, phone_no, address, email, sme_status) VALUES (NULL, \"" . $company_name . '" , "' . $phone_no . '", "' . $address . '", "' . $email . '", ' . '"active");';
+
+        if ($conn->query($qry2) === true) {
+            echo '<script>alert("Successfully Registered");</script>';
+        } else {
+            echo "Error: " . $qry2 . "<br>" . $conn->error;
+        }
+    } else {
+        echo "Error: " . $qry1 . "<br>" . $conn->error;
+    }
+}
+
 if (isset($_POST['SubmitButton'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $message = "SELECT * FROM login WHERE email = " . '"' . $username . '"' . " AND password = " . '"' . $password . '";';
-    
+
     $conn = mysqli_connect('localhost', 'root', '', 'cw2');
     $query = "SELECT * FROM login WHERE email = " . '"' . $username . '"' . " AND password = " . '"' . $password . '";';
     $result = mysql_query($query);
@@ -17,14 +40,13 @@ if (isset($_POST['SubmitButton'])) {
     if ($obj) {
         echo '<script> sessionStorage.setItem("userType", "' . $obj->userType . '");</script>';
         var_dump($obj->userType);
-        // header('Location: index.php/main/area');
+        header('Location: index.php/main/area');
     } else {
         printf("Invalid username or password : ");
     }
-
-    // echo "<script>console.log('Debug Objects: " . "data" . $result->fetch_array()[0] . "' );</script>";
 }
 
+// echo "<script>console.log('Debug Objects: " . "data" . $result->fetch_array()[0] . "' );</script>";
 ?>
 
 
@@ -32,7 +54,29 @@ if (isset($_POST['SubmitButton'])) {
 <html>
 
 <body>
-    <form action="" method="post">
+    <form id="smeSignupForm" action="" method="post">
+        <?php echo $message; ?>
+
+        <label for="company_name">Company Name:</label>
+        <input type="text" id="company_name" name="company_name"><br>
+
+        <label for="phone_no">Phone Number:</label>
+        <input type="tel" id="phone_no" name="phone_no"><br>
+
+        <label for="address">Address:</label>
+        <textarea id="address" name="address"></textarea><br>
+
+        <label for="email">Email:</label>
+        <input id="email" name="email"><br>
+
+        <label for="password">Password:</label>
+        <input id="password" name="password"><br>
+
+        <br>
+
+        <input type="submit" name="SubmitSmeSignupBtn" />
+    </form>
+    <form id="loginForm" action="" method="post">
         <?php echo $message; ?>
         <label for="username">Username:</label>
         <input type="text" name="username" id="username" required>
@@ -45,3 +89,8 @@ if (isset($_POST['SubmitButton'])) {
 </body>
 
 </html>
+<script>
+if (window.history.replaceState) {
+    window.history.replaceState(null, null, window.location.href);
+}
+</script>
